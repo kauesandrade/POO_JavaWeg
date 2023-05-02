fdb4e28acef084c8bd8d623e786a2b7fda515e6c
kauzzx <kauesandrade206@gmail.com>
2023-05-02 16:52:44 -0300

    02/05/2023

---
+++ Escola/src/Secretaria/Avaliacao.java
@@ -0,0 +1,10 @@
+package Secretaria;
+
+public class Avaliacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Escola/src/Secretaria/Unidades_curriculares.java
@@ -0,0 +1,10 @@
+package Secretaria;
+
+public class Unidades_curriculares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
