6baf8a6b9469762a3b08052ef77fbcc1d488c7ed
kauzzx <kauesandrade206@gmail.com>
2023-06-18 17:51:42 -0300

    a

--- Atividade 2 - Estudo de caso/Programa/ECommerce/src/model/Mainteste.java
+++ Atividade 2 - Estudo de caso/Programa/ECommerce/src/model/Mainteste.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -18,19 +17,32 @@ import javax.swing.JTextField;
 
 public class Mainteste {
 	
-	private static Scanner sc = new Scanner(System.in);
 	private static ArrayList<Cliente> clientes = new ArrayList();
-	private static  Cliente cliente = new Cliente();
+	private static JComboBox<String> cbProdutos = new JComboBox<String>();
+	private static ArrayList<Produto> produtos = new ArrayList<Produto>();
+	private static JComboBox<String> cbCategorias = new JComboBox<String>();
+	private static Cliente cliente = new Cliente();
+	private static ArrayList<Produto> carrinhoProdutos = new ArrayList<Produto>();
 	private static int logado;
+	private static int selecProduto = 0;
 	
 	public static void main(String[] args) {
 	
-	 
+	// !!!________PARA FAZER_______!!!
+	 // !!!!!!!!!!!!!!!!!!! SE O CLIENTE CLICAR NA OPCCAO DE ESCREVER, REMOVER PARA ELE ESCREVER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//------------------!!!!!!!!!!Mudar para que n selecione uma opcao que n tenha nd_______ metodo comprar!!!!!!!!!!!!!!
+		
 	 //--------------Tela Inicio-----------
 		
 		cliente.setLogin("kaue");
 		cliente.setSenha("123123");
 		clientes.add(cliente);
+		
+		cbCategorias.addItem("Esportes");
+		cbCategorias.addItem("Infantil");
+		cbCategorias.addItem("Tecnologia");
+		cbCategorias.addItem("Lazer");
+		cbCategorias.addItem("Cozinha");
 		
 		telaInicial();
 		
@@ -43,7 +55,7 @@ public class Mainteste {
 		JButton btComprar = new JButton("Comprar");
 		JButton btCarrinho = new JButton("Carrinho");
 		JButton btProduto = new JButton("Produto");
-		Object[] LayoutTelaPrincipal = {btComprar, btCarrinho, btProduto};
+		Object[] layoutTelaPrincipal = {btComprar, btCarrinho, btProduto};
 		
 	
 	btComprar.addActionListener((ActionListener) new ActionListener(){
@@ -54,36 +66,24 @@ public class Mainteste {
 	
 	btCarrinho.addActionListener((ActionListener) new ActionListener(){
 		public void actionPerformed(ActionEvent e) {
-			editProduto();
+			carrinho();
 		}
 	});
 	
 	btProduto.addActionListener((ActionListener) new ActionListener(){
 		public void actionPerformed(ActionEvent e) {
-			
+			produto();
 		}
 	});
 	
 	
-	int opcao = JOptionPane.showInternalConfirmDialog(null, LayoutTelaPrincipal, "Tela Principal", JOptionPane.DEFAULT_OPTION);
+	int opcao = JOptionPane.showInternalConfirmDialog(null, layoutTelaPrincipal, "Tela Principal", JOptionPane.CLOSED_OPTION);
 	
 	if(opcao == -1){
 		
 	}
 	
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public static void telaInicial() {
 		JButton btCadastro = new JButton("Cadastrar");
@@ -132,39 +132,38 @@ public class Mainteste {
 		JLabel lbCpf = new JLabel("CPF:");
 		JLabel lbDataNas = new JLabel("Data de Nascimento:");
 
-		JTextField nome = new JTextField();
-		JTextField login = new JTextField();
-		JPasswordField senha = new JPasswordField();
-		JTextField email = new JTextField();
-		JTextField cpf = new JTextField();
-		JTextField dataNas = new JTextField();
-		Object[]layoutCadastro = {lbNome, nome, lbLogin, login, lbSenha, senha, lbEmail, email, lbCpf, cpf, lbDataNas, dataNas};
-		 
-		String logar;
+		JTextField txNome = new JTextField();
+		JTextField txLogin = new JTextField();
+		JPasswordField txSenha = new JPasswordField();
+		JTextField txEmail = new JTextField();
+		JTextField txCpf = new JTextField();
+		JTextField txDataNas = new JTextField();
+		Object[]layoutCadastro = {lbNome, txNome, lbLogin, txLogin, lbSenha, txSenha, lbEmail, txEmail, lbCpf, txCpf, lbDataNas, txDataNas};
 		 
 		int opcao = JOptionPane.showInternalConfirmDialog(null, layoutCadastro, "Cadastro", JOptionPane.OK_CANCEL_OPTION);
 
-		String nome1 = nome.getText();
-		String login1 = login.getText();
-		String senha1 = String.valueOf(senha.getPassword());
-		String email1 = email.getText();
-		String cpf1 = cpf.getText();
-		String dataNas1 = dataNas.getText();
+		String nome = txNome.getText();
+		String login = txLogin.getText();
+		String senha = String.valueOf(txSenha.getPassword());
+		String email = txEmail.getText();
+		String cpf = txCpf.getText();
+		String dataNas = txDataNas.getText();
 			
 			 
 		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-		Date data = formato.parse(dataNas1);
+		Date data = formato.parse(dataNas);
 			
-		logar = cliente.cadastrar(nome1, login1, senha1, cpf1, data, email1);
+		String cadastrar = cliente.cadastrar(nome, login, senha, cpf, data, email);
 		
 		if(opcao == -1) {
 			
 		}
 		else {
-			if(logar != "A") {
-				JOptionPane.showMessageDialog(null, logar, "Erro", JOptionPane.DEFAULT_OPTION);
+			if(cadastrar != "A") {
+				JOptionPane.showMessageDialog(null, cadastrar, "Erro", JOptionPane.DEFAULT_OPTION);
+				cadastro();
 			}
-			else if(logar.equals("A")){
+			else if(cadastrar.equals("A")){
 				JOptionPane.showMessageDialog(null, "Cliente Cadastrado", "Confirmado", JOptionPane.DEFAULT_OPTION);
 				clientes.add(cliente);
 					
@@ -178,8 +177,8 @@ public class Mainteste {
 		
 	 JLabel lblogin = new JLabel("Login");
 	 JLabel lbsenha = new JLabel("Senha");
-	 JTextField login = new JTextField();
-	 JPasswordField senha = new JPasswordField();
+	 JTextField login = new JTextField("kaue");
+	 JPasswordField senha = new JPasswordField("123123");
 	 
 	 
 	 Object[] layoutLogin = {lblogin, login, lbsenha, senha};
@@ -220,33 +219,27 @@ public class Mainteste {
 	public static void comprar() {
 		
 		JLabel lbCategoria = new JLabel("Categoria: ");
-		JComboBox cbCategoria = new JComboBox();
-		cbCategoria.addItem("Esportes");
-		cbCategoria.addItem("Infantil");
-		cbCategoria.addItem("Tecnologia");
-		cbCategoria.addItem("Lazer");
-		cbCategoria.addItem("Cozinha");
 		
 		
+		Object[] layoutComprar = {lbCategoria, cbCategorias};
+		int opcao = JOptionPane.showInternalConfirmDialog(null, layoutComprar, "Categoria", JOptionPane.DEFAULT_OPTION);
 		
-		Object[] componentes = {lbCategoria, cbCategoria};
-		int opcao = JOptionPane.showInternalConfirmDialog(null, componentes, "Categoria", JOptionPane.DEFAULT_OPTION);
 		
-		
-		String categoria = cbCategoria.getSelectedItem().toString();
-		ArrayList<Produto> produtos = new ArrayList<Produto>();
-		JComboBox<String> comboBox1 = new JComboBox<String>();
+		String categoria = cbCategorias.getSelectedItem().toString();
+		JComboBox<String> cbArr = new JComboBox<String>();
 		
 		for(int i = 0; i < produtos.size(); i++) {
-			if(Produto.getCategoria().equals(categoria)) {
-				comboBox1.addItem(produtos.get(i).getNome());
+			
+			if(produtos.get(i).getCategoria().equals(categoria)) {
+				cbArr.addItem(produtos.get(i).getNome());
 			}
 		}
 		
-//		System.out.println(opcao);
-		
+		String selecNomeProduto = "N";
+
 		if( produtos.size() > 0) {
-			JOptionPane.showMessageDialog(null, comboBox1, ("Categoira: "+categoria), JOptionPane.DEFAULT_OPTION);
+			JOptionPane.showMessageDialog(null, cbArr, ("Categoira: "+categoria), JOptionPane.DEFAULT_OPTION);
+			selecNomeProduto = cbArr.getSelectedItem().toString();
 			}
 		else if(opcao == 0) {
 			JOptionPane.showMessageDialog(null, "Não há Produtos nessa categoria", ("Categoira: "+categoria), JOptionPane.DEFAULT_OPTION);
@@ -255,21 +248,221 @@ public class Mainteste {
 		else if(opcao == -1)   {
 			
 			}
+
+		if(selecNomeProduto != "N") {
+			for(int i = 0; i < produtos.size(); i++) {
+				if(produtos.get(i).getNome().equals(selecNomeProduto)) {
+					selecProduto = i;
+				}
+			}
+			mostrarProduto();
+			
+		}
 			
 		}
 		
+	public static void carrinho() {
+		
+		ArrayList<String> produtosCarrinhoInfo = new ArrayList<String>();
+		
+		
+		for(int i = 0; i < carrinhoProdutos.size(); i++) {
+			
+			produtosCarrinhoInfo.add("Nome: "+ carrinhoProdutos.get(i).getNome() +
+					"\nPreço: "+ carrinhoProdutos.get(i).getPreco() +
+					"\nQuantidade: 1\n");
+		}
+		
+		JButton btFinalizarCompra = new JButton("Finalizar compra");
+		JButton btRemover = new JButton("Remover itens");
+		JButton btVoltar = new JButton("Voltar");
+		
+		Object[] layoutCarrinho = {produtosCarrinhoInfo, btFinalizarCompra, btRemover, btVoltar};
+		
+		btFinalizarCompra.addActionListener((ActionListener) new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btRemover.addActionListener((ActionListener) new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				removerDoCarrinho();
+			}
+		});
+		btVoltar.addActionListener((ActionListener) new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				telaPrincipal();
+			}
+		});
+		
+		JOptionPane.showInternalConfirmDialog(null, layoutCarrinho, "Carrinho", JOptionPane.DEFAULT_OPTION);
+		
+		
+	}
+		
+	public static void produto() {
+		
+		JButton btEditProduto = new JButton("Editar Produto");
+		JButton btCadastrarProduto = new JButton("Cadastrar Produto");
+		
+		Object[] layoutPoduto = {btCadastrarProduto, btEditProduto}; 
+		
+		btEditProduto.addActionListener((ActionListener) new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+			editProduto();
+		}
+	});
+	
+		btCadastrarProduto.addActionListener((ActionListener) new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				cadastrarProduto();
+		}
+	});
+		
+	JOptionPane.showInternalConfirmDialog(null, layoutPoduto, "Produto", JOptionPane.DEFAULT_OPTION);
+		
+	}
+		
+	
 	public static void editProduto() {
 		
-		JButton btFinalizarCompra = new JButton("Comprar");
-		JButton btCarrinho = new JButton("Carrinho");
-		JButton btProduto = new JButton("Produto");
-		Object[] LayoutTelaPrincipal = {btComprar, btCarrinho, btProduto};
-	}
+		JOptionPane.showInternalConfirmDialog(null, cbProdutos, "Produto", JOptionPane.DEFAULT_OPTION);
+		String produtoSelecionado = cbProdutos.getSelectedItem().toString();
 		
-		
+		System.out.println(produtoSelecionado);
 		
 		
 	}
+	
+	public static void cadastrarProduto() {
+		
+		JLabel lbNome = new JLabel("Nome do produto");
+		JLabel lbPreco = new JLabel("Preço");
+		JLabel lbCategoria = new JLabel("Categoria");
+		JLabel lbModelo = new JLabel("Modelo");
+		JLabel lbMarca = new JLabel("Marca");
+		JLabel lbCor = new JLabel("Cor");
+		JLabel lbDescricao = new JLabel("Descrição");
+		JLabel lbLimite = new JLabel("Limite de comprar por pessoa");
+		JLabel lbQuantidade = new JLabel("Quantidade no estoque");
+		
+		JTextField txNome = new JTextField("Camisa");
+		JTextField txPreco = new JTextField("10");
+		JTextField txModelo = new JTextField("1");
+		JTextField txMarca = new JTextField("1");
+		JTextField txCor = new JTextField("1");
+		JTextField txDescricao = new JTextField("1");
+		JTextField txLimite = new JTextField("1");
+		JTextField txQuantidade = new JTextField("1");
+		Object[] layoutCadastrarProduto = {lbNome, txNome, lbPreco, txPreco, lbCategoria, cbCategorias,lbModelo,
+				txModelo,lbMarca, txMarca, lbCor, txCor, lbDescricao, txDescricao, lbLimite, txLimite, lbQuantidade, txQuantidade};
+		
+		int opcao = JOptionPane.showConfirmDialog(null, layoutCadastrarProduto, "Cadastro de Produto", JOptionPane.CANCEL_OPTION);
+		
+		Produto produto = new Produto();
+		
+		String nome = txNome.getText();
+		Double preco = Double.parseDouble(txPreco.getText());
+		String categoria = cbCategorias.getSelectedItem().toString();
+		String modelo = txModelo.getText();
+		String marca = txMarca.getText();
+		String cor = txCor.getText();
+		String descricao = txDescricao.getText();
+		int limite = Integer.parseInt(txLimite.getText());
+		int quantidade = Integer.parseInt(txQuantidade.getText());
+		
+		String cadasProduto = produto.CadastrarProduto( nome, preco, categoria, modelo,
+				marca, cor, descricao, limite, quantidade);
+		
+		if(opcao == -1){
+			
+		}else {
+			if(cadasProduto.equals("A")) {
+				produtos.add(produto);
+				JOptionPane.showConfirmDialog(null, "Produto Cadastrado!!!", "Confirmado", JOptionPane.CANCEL_OPTION);
+			}
+			else if(cadasProduto != "A") {
+				JOptionPane.showConfirmDialog(null, cadasProduto, "Erro", JOptionPane.CANCEL_OPTION);
+				cadastrarProduto();
+			}
+		}
+		
+	}
+	
+	public static void mostrarProduto() {
+		
+		JLabel lbNome = new JLabel("Nome: "+produtos.get(selecProduto).getNome());
+		JLabel lbPreco = new JLabel("Preço: "+produtos.get(selecProduto).getPreco());
+		JLabel lbModelo = new JLabel("Modelo: "+produtos.get(selecProduto).getModelo());
+		JLabel lbMarca = new JLabel("Marca: "+produtos.get(selecProduto).getMarca());
+		JLabel lbCor = new JLabel("Cor: "+produtos.get(selecProduto).getCor());
+		JLabel lbDescricao = new JLabel("Descrição: "+produtos.get(selecProduto).getDescricao());
+		JButton btComprar = new JButton("Comprar");
+		
+		Object[] layoutInfoProduto = {lbNome, lbPreco, lbModelo, lbMarca, lbCor, lbDescricao, btComprar};
+		
+		btComprar.addActionListener((ActionListener) new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mostrarQuantidade();
+			}
+		});
+		
+		JOptionPane.showMessageDialog(null, layoutInfoProduto, "Produto", JOptionPane.DEFAULT_OPTION);
+		
+		
+
+	}
+	
+	public static void mostrarQuantidade() {
+		JLabel lbQuantideLimite = new JLabel("Quantidade limite: "+produtos.get(selecProduto).getLimiteDeCompras());
+		JLabel lbQuantidadeEstoque = new JLabel("Quantiade no estoque: "+produtos.get(selecProduto).getQuantidadeEstoque());
+		JLabel lbQuantidade = new JLabel("Digite a quantidade que deseja");
+		JTextField txQuantidade = new JTextField("1");
+		Object[] layoutQuantidadeCompra = {lbQuantideLimite, lbQuantidadeEstoque, lbQuantidade, txQuantidade};
+		
+		int opcao = JOptionPane.showInternalConfirmDialog(null, layoutQuantidadeCompra, "Quantidade", JOptionPane.CLOSED_OPTION);
+		
+		int quantidade = Integer.parseInt(txQuantidade.getText()); 
+		
+		if(opcao == -1) {
+			
+		}else {
+				if(quantidade <= produtos.get(selecProduto).getQuantidadeEstoque()&& quantidade <= produtos.get(selecProduto).getLimiteDeCompras()) {
+					produtos.get(selecProduto).setQuantidadeEstoque(produtos.get(selecProduto).getQuantidadeEstoque() - quantidade);
+					JOptionPane.showMessageDialog(null, "Produto adicionado no seu carrinho!!!", "Confirmado", JOptionPane.DEFAULT_OPTION);
+					carrinhoProdutos.add(produtos.get(selecProduto));
+				}else {
+					if(quantidade > produtos.get(selecProduto).getQuantidadeEstoque()){
+						JOptionPane.showMessageDialog(null, "Não temos essa quantidade no estoque!!!", "Erro", JOptionPane.ERROR_MESSAGE);
+						mostrarQuantidade();
+					}else if(quantidade > produtos.get(selecProduto).getLimiteDeCompras()) {
+						JOptionPane.showMessageDialog(null, "Digite uma quantidade menor ou igual ao limite de compra!!!", "Erro", JOptionPane.ERROR_MESSAGE);
+						mostrarQuantidade();
+					}
+				}
+			
+		}
+	}
+	
+	
+	
+	private static void removerDoCarrinho() {
+		JComboBox<String> cbArr = new JComboBox<String>();
+		
+		for(int i = 0; i < carrinhoProdutos.size(); i++) {
+			cbArr.addItem(carrinhoProdutos.get(i).getNome());
+		}
+		JLabel titulo = new JLabel("Escolha um para remover");
+		
+		Object[] layoutRemoverDoCarrinho = {cbArr, titulo};
+		
+		JOptionPane.showConfirmDialog(null, layoutRemoverDoCarrinho, "Remover", JOptionPane.DEFAULT_OPTION );
+	}
+	
+	
+	}
+
 	
 	
 	
--- Atividade 2 - Estudo de caso/Programa/ECommerce/src/model/Produto.java
+++ Atividade 2 - Estudo de caso/Programa/ECommerce/src/model/Produto.java
@@ -1,21 +1,77 @@
 package model;
 
-import java.awt.Component;
 import java.util.ArrayList;
 
 public class Produto {
 
+	public Produto() {
+		super();
+		
+	}
+	
 	private String nome;
 	private Double preco;
-	private static String categoria;
-	private String avaliacao;
+	private String categoria;
+	private int avaliacao;
 	private String modelo;
 	private String marca;
 	private String cor;
 	private String descricao;
 	private String fotos;
 	private int limiteDeCompras;
+	private int quantidadeEstoque;
 	
+	public Produto(String nome, Double preco, int avaliacao, String modelo, String marca, String cor,
+			String descricao, String fotos, int limiteDeCompras, String categoria, int quantidadeEstoque) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.categoria = categoria;
+		this.avaliacao = avaliacao;
+		this.modelo = modelo;
+		this.marca = marca;
+		this.cor = cor;
+		this.descricao = descricao;
+		this.fotos = fotos;
+		this.limiteDeCompras = limiteDeCompras;
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	
+	public String CadastrarProduto(String nome, Double preco, String categoria, String modelo,
+			String marca, String cor, String descricao, int limiteDeCompras, int quantidadeEstoque) {
+		setNome(nome);
+		setPreco(preco);
+		setCategoria(categoria);
+		setModelo(modelo);
+		setMarca(marca);
+		setCor(cor);
+		setDescricao(descricao);
+		setLimiteDeCompras(limiteDeCompras);
+		setQuantidadeEstoque(quantidadeEstoque);
+		
+		if(getNome() == null) {
+			return "Digite um Nome valido!";
+		}else if(getPreco() == null) {
+			return "Digite um Preco valido!";
+		}else if(getCategoria() == null) {
+			return "Escolha uma Categoria valida!";
+		}else if(getModelo() == null) {
+			return "Digite um Modelo valido!";
+		}else if(getMarca() == null) {
+			return "Digite uma Marca valida!";
+		}else if(getCor() == null){
+			return "Digite uma Cor valida!";
+		}else if(getDescricao() == null) {
+			return "Digite uma Descrição valida!";
+		}else if(getLimiteDeCompras() == 0) {
+			return "Digite um Limite de compras valido!";
+		}else if(getQuantidadeEstoque() == 0) {
+			return "Digite uma Quantide maior que 0";
+		}
+		return "A";
+		
+	}
+
 	public String getNome() {
 		return nome;
 	}
@@ -28,16 +84,16 @@ public class Produto {
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-	public static String getCategoria() {
+	public String getCategoria() {
 		return categoria;
 	}
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-	public String getAvaliacao() {
+	public int getAvaliacao() {
 		return avaliacao;
 	}
-	public void setAvaliacao(String avaliacao) {
+	public void setAvaliacao(int avaliacao) {
 		this.avaliacao = avaliacao;
 	}
 	public String getModelo() {
@@ -76,5 +132,11 @@ public class Produto {
 	public void setLimiteDeCompras(int limiteDeCompras) {
 		this.limiteDeCompras = limiteDeCompras;
 	}
-	
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
 }
