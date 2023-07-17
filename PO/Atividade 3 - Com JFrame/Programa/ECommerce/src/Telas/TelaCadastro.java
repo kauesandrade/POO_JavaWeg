c98cf986625927d349e3a9b08f37578c6be8b56d
kauzzx <128431286+kauzzx@users.noreply.github.com>
2023-07-17 19:45:10 -0300

    a

--- PO/Atividade 3 - Com JFrame/Programa/ECommerce/src/Telas/TelaCadastro.java
+++ PO/Atividade 3 - Com JFrame/Programa/ECommerce/src/Telas/TelaCadastro.java
@@ -49,7 +49,7 @@ public class TelaCadastro extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(109, 152, 186));
+		contentPane.setBackground(new Color(0, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
--- PO/Atividade 3 - Com JFrame/Programa/ECommerce/src/Telas/TelaPrincipal.java
+++ PO/Atividade 3 - Com JFrame/Programa/ECommerce/src/Telas/TelaPrincipal.java
@@ -16,6 +16,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class TelaPrincipal extends JFrame {
 
@@ -39,22 +41,64 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
+		getContentPane().setBackground(new Color(0, 128, 128));
+		getContentPane().setForeground(new Color(0, 128, 192));
+		getContentPane().setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 434, 261);
+		getContentPane().add(tabbedPane);
+		
+		JPanel telaPricipal = new JPanel();
+		tabbedPane.addTab("Tela Principal", null, telaPricipal, null);
+		
+		JPanel carrinho = new JPanel();
+		tabbedPane.addTab("Carrinho", null, carrinho, null);
+		carrinho.setLayout(null);
+		
+		JButton btRemoverItens = new JButton("Remover Itens");
+		btRemoverItens.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 11));
+		btRemoverItens.setBounds(60, 199, 155, 23);
+		carrinho.add(btRemoverItens);
+		
+		JButton btFinalizarCompra = new JButton("Finalizar Compra");
+		btFinalizarCompra.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 11));
+		btFinalizarCompra.setBounds(225, 199, 155, 23);
+		carrinho.add(btFinalizarCompra);
+		
+		JLabel lbCarrinho = new JLabel("a");
+		lbCarrinho.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 11));
+		lbCarrinho.setBounds(21, 11, 267, 177);
+		carrinho.add(lbCarrinho);
+		
+		JLabel lbIconCarrinho = new JLabel("");
+		lbIconCarrinho.setIcon(new ImageIcon("C:\\Users\\kaue_s_andrade\\Documents\\GitHub\\PO_Java\\PO\\Atividade 3 - Com JFrame\\assets\\carrinho-de-compras.png"));
+		lbIconCarrinho.setBounds(274, 23, 131, 150);
+		carrinho.add(lbIconCarrinho);
+		
+		JPanel perfil = new JPanel();
+		tabbedPane.addTab("Perfil", null, perfil, null);
+		tabbedPane.setBackgroundAt(2, new Color(0, 128, 128));
+		perfil.setLayout(null);
+		
+		JLabel lbUsuario = new JLabel("Ne");
+		lbUsuario.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 11));
+		lbUsuario.setBounds(10, 11, 296, 195);
+		perfil.add(lbUsuario);
+		
+		JButton btnNewButton = new JButton("Editir Perfil");
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setBackground(Color.WHITE);
+		btnNewButton.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 11));
+		btnNewButton.setBounds(301, 199, 118, 23);
+		perfil.add(btnNewButton);
+		
+		JLabel lbIconPerfil = new JLabel("");
+		lbIconPerfil.setIcon(new ImageIcon("C:\\Users\\kaue_s_andrade\\Documents\\GitHub\\PO_Java\\PO\\Atividade 3 - Com JFrame\\assets\\image-removebg-preview (1).png"));
+		lbIconPerfil.setBounds(316, 44, 103, 127);
+		perfil.add(lbIconPerfil);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JLabel lblNewLabel = new JLabel("KAUE SHOP");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\kaue_s_andrade\\Downloads\\5903430 (1).png"));
-		lblNewLabel.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 11));
-		menuBar.add(lblNewLabel);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
-		menuBar.add(mntmNewMenuItem_1);
-		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
-		menuBar.add(mntmNewMenuItem_2);
 	}
 
 	private static void addPopup(Component component, final JPopupMenu popup) {
