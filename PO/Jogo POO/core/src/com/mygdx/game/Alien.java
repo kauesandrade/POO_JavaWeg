5b2d34626d8c781150f5e4ec585cebe01ec70f9f
kauzzx <128431286+kauzzx@users.noreply.github.com>
2023-10-03 16:50:35 -0300

    Update Alien.java

--- PO/Jogo POO/core/src/com/mygdx/game/Alien.java
+++ PO/Jogo POO/core/src/com/mygdx/game/Alien.java
@@ -1,21 +1,20 @@
-//package com.mygdx.game;
-//
-//import java.util.Iterator;
-//
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.Input;
-//import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.g2d.Sprite;
-//import com.badlogic.gdx.math.MathUtils;
-//import com.badlogic.gdx.math.Rectangle;
-//import com.badlogic.gdx.utils.Array;
-//
-//public class Alien {
-//
-//	private Array<Rectangle> aliens = new Array<Rectangle>();
-//	private Texture imgAlien = new Texture("meteoro.png");
-//	
-//	
-//	
-//}
-//
+package com.mygdx.game;
+
+import java.util.Iterator;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
+public class Alien {
+
+	private Array<Rectangle> aliens = new Array<Rectangle>();
+	private Texture imgAlien = new Texture("meteoro.png");
+	
+	
+	
+}
